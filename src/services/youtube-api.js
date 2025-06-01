96c4da16fa7eadd5f274defec00d22e2f19ef513
Rick Hogan <Rhogan1418@gmail.com>
2025-06-01 00:25:19 -0700

    Cleanup youtube-api requests

--- src/services/youtube-api.js
+++ src/services/youtube-api.js
@@ -5,12 +5,20 @@ const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 const API_BASE = 'https://www.googleapis.com/youtube/v3';
 
 export const getSearchResults = async (query, sortBy, pageToken, signal) => {
-    let url = `${API_BASE}/search?q=${encodeURIComponent(query)}&maxResults=25&key=${API_KEY}&part=snippet&order=${sortBy}&type=video`;
+     const params = new URLSearchParams({
+        q: query,
+        maxResults: 25,
+        key: API_KEY,
+        part: 'snippet',
+        order: sortBy,
+        type: 'video',
+    });
+
     if (pageToken) {
-        url = url + '&pageToken=' + pageToken;
+        params.set('pageToken', pageToken);
     }
-    const res = await fetch(url, { signal });
-   
+    
+    const res = await fetch(`${API_BASE}/search?${params.toString()}`, { signal });
     const data = await res.json();
 
     if (data.error) {
@@ -21,8 +29,13 @@ export const getSearchResults = async (query, sortBy, pageToken, signal) => {
 }
 
 export const getVideoStatisticResults = async (videoIdList, signal) => {
-    const res = await fetch(`${API_BASE}/videos?id=${videoIdList.join(',')}&part=statistics&key=${API_KEY}`, { signal });
-
+    const params = new URLSearchParams({
+        id: videoIdList.join(','),
+        part: 'statistics',
+        key: API_KEY
+    });
+    
+    const res = await fetch(`${API_BASE}/videos?${params.toString()}`, { signal });
     const data = await res.json();
 
     if (data.error) {
